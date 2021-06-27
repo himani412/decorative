3f1a7916a7a459ddd4b7fcb5ad20e00fad1091b4
himani412 <77782996+himani412@users.noreply.github.com>
2021-06-26 23:14:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(150,150);
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,230,150,220,200);
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    rect(90,40,460,20);
+
+    
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(300,50,80);
+
+    fill(0,128,0);
+    stroke(255,0,0);
+    rect(550,65,20,460);
+
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(550,50,80);
+
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(550,250,80);
+
+
+    fill(0,128,0);
+    stroke(255,0,0);
+    rect(90,40,20,460);
+
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(550,430,80);
+
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(350,420,80);
+
+
+    fill(0,128,0);
+    stroke(255,0,0);
+    rect(90,420,460,20);
+
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(90,420,80);
+
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(90,250,80);
+
+
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(80,50,80);
+}
+function take_snapshot(){
+    save('Himani.png');
+}
